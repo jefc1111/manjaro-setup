31ca2544104de2652bfba592347bf1579bda73c4
Geoff Clayton <47053963+jefc1111@users.noreply.github.com>
2020-12-29 14:31:01 +0000

    Add files via upload

---
+++ config-files/after-startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+i3-msg [class="Brave-browser"] move container to workspace 2
+i3-msg [class="Slack"] move container to workspace 3
+i3-msg [class="code-oss"] move container to workspace 4
